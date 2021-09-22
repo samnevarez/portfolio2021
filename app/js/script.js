119e23c7d42912cff016285db04052a2553edd41
ORBITVL <ORBITVL@Sammuels-iMac.local>
2021-09-22 16:38:02 -0700

    before gulp build

--- app/js/script.js
+++ app/js/script.js
@@ -9,7 +9,7 @@
       if (target.length) {
         $('html, body').animate({
           scrollTop: (target.offset().top - 70)
-        }, 100, "");
+        }, 0, "easeInOutExpo");
         return false;
       }
     }
