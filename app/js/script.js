cc63e1036276756a3a9d43abbe16afa299fb82f1
ORBITVL <ORBITVL@Sammuels-iMac.local>
2021-09-22 02:52:35 -0700

    edits to main

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,42 @@
-const testString = 'This is a test!';
+(function($) {
+  "use strict"; // Start of use strict
 
-console.log(testString);
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 70)
+        }, 100, "");
+        return false;
+      }
+    }
+  });
 
-const fruits = [
-  'mango',
-  'papaya',
-  'lychee',
-];
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
 
-fruits.map((fruit) => {
-  console.log(fruit);
-});
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 100
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+})(jQuery); // End of use strict
